7ae11fd5b797a0b2bf9b56f5791afb382297b3cb
Sonia Han <36926546+soheunhan@users.noreply.github.com>
2024-03-13 16:00:23 -0400

    initial prom-client setup

--- server/server.js
+++ server/server.js
@@ -1,8 +1,27 @@
+('use strict');
+
 const express = require('express');
 const path = require('path');
 
 const app = express();
 const PORT = 3000;
+
+//Configuring prom-client
+const client = require('prom-client');
+const collectDefaultMetrics = client.collectDefaultMetrics;
+//Probe every 5th second.
+collectDefaultMetrics({ timeout: 5000 });
+
+const counter = new client.Counter({
+  name: 'node_request_operations_total',
+  help: 'The total number of processed requests',
+});
+
+const histogram = new client.Histogram({
+  name: 'node_request_duration_seconds',
+  help: 'Histogram for the duration in seconds.',
+  buckets: [1, 2, 5, 6, 10],
+});
 
 app.use('/dist', express.static(path.resolve(__dirname, '../dist')));
 
@@ -10,6 +29,12 @@ app.get('/', (req, res) => {
   return res
     .status(200)
     .sendFile(path.join(__dirname, '../client/public/index.html'));
+});
+
+//set up metrics endpoint
+app.get('/metrics', (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(client.register.metrics());
 });
 
 app.listen(PORT, () => {
