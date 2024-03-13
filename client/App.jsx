fe38e0ee572f21becc1229d93dd1eb4d35e63f9a
Jinseong Nam <jinseongnam@Jinseongs-MacBook-Air.local>
2024-03-13 15:16:53 -0400

    pushing

--- client/App.jsx
+++ client/App.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import './stylesheets/styles.scss';
-import WelcomePage from './pages/WelcomePage';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import NotFoundPage from './pages/NotFoundPage.jsx';
+import WelcomePage from './pages/WelcomePage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <WelcomePage />,
+    errorElement: <NotFoundPage />,
+  },
+]);
 
 export default function App() {
-  return <WelcomePage />;
+  return <RouterProvider router={router} />;
 }
--- client/index.js
+++ client/index.js
@@ -2,21 +2,12 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createRoot } from 'react-dom/client';
 import store from './slices/store.js';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import WelcomePage from './pages/WelcomePage.jsx';
-import NotFoundPage from './pages/NotFoundPage.jsx';
+import App from './App.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <WelcomePage />,
-    errorElement: <NotFoundPage />,
-  },
-]);
 
 const rootElement = createRoot(document.getElementById('root'));
 rootElement.render(
   <Provider store={store}>
-    <RouterProvider router={router} />
+    <App />
   </Provider>
 );
