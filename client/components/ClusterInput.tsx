4494f93dc313afe14e9577d85b8af3d778443b76
Jinseong Nam <jinseongnam@Jinseongs-MacBook-Air.local>
2024-03-19 14:21:52 -0400

    change to input, css, and structure of types for db and server

--- client/components/ClusterInput.tsx
+++ client/components/ClusterInput.tsx
@@ -11,11 +11,11 @@ const ClusterInput = (props: ClusterInputProps) => {
   return (
     <div className="mb-6">
       <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-        Your Prometheus URL
       </label>
       <input
         type="text"
         id="default-input"
+        placeholder='Your Prometheus URL'
         onChange={props.handleInputChange}
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
       />
