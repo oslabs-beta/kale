6e5e877cdf4b942db1d300abd14d5c9b0fef4b36
Jeff C <jeffplv@gmail.com>
2024-04-01 11:25:21 -0400

    changed welcome page back

--- client/pages/Signup.tsx
+++ client/pages/Signup.tsx
@@ -13,13 +13,16 @@ const SignupContainer = () => {
   const [signup] = useSignupMutation();
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
   const [loginData, setLoginData] = useState<VerifyData>({
     firstName: '',
     email: '',
     password: '',
     confirmPassword: '',
   });
-
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
   const [passwordError, setPasswordError] = useState<string>('');
   const [confirmPasswordError, setConfirmPasswordError] = useState<string>('');
 
@@ -126,17 +129,17 @@ const SignupContainer = () => {
               <form onSubmit={submitHandler} className='space-y-4 md:space-y-6'>
                 <div>
                   <label
-                    htmlFor='username'
+                    htmlFor='firstName'
                     className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
                   >
                     Your First Name
                   </label>
                   <input
-                    type='username'
+                    type='firstName'
                     name='firstName'
                     id='firstName'
                     className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-zinc-700 dark:border-zinc-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-                    placeholder='username'
+                    placeholder='First Name'
                     required
                     value={loginData.firstName}
                     onChange={handleChange}
@@ -168,7 +171,7 @@ const SignupContainer = () => {
                     Password
                   </label>
                   <input
-                    type='password'
+                    type={showPassword ? 'text' : 'password'}
                     name='password'
                     id='password'
                     placeholder='Password'
@@ -178,6 +181,13 @@ const SignupContainer = () => {
                     onChange={handleChange}
                   />
                 </div>
+                {/* <div className='text-right mt-0'> */}
+                <p>
+                  Create a strong password including upper and lower case
+                  letters, numbers, symbols and 8 characters long.
+                </p>
+                {/* </div> */}
+
                 <div>
                   <label
                     htmlFor='confirm-password'
@@ -186,7 +196,7 @@ const SignupContainer = () => {
                     Confirm password
                   </label>
                   <input
-                    type='password'
+                    type={showPassword ? 'text' : 'password'}
                     name='confirmPassword'
                     id='confirm-password'
                     placeholder='Confirm'
@@ -195,39 +205,21 @@ const SignupContainer = () => {
                     onChange={handleChange}
                   />
                 </div>
-                <div className='flex items-start'>
-                  <div className='flex items-center h-5'>
-                    <input
-                      id='terms'
-                      aria-describedby='terms'
-                      type='checkbox'
-                      className='w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-zinc-700 dark:border-zinc-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800'
-                      required
-                    />
-                  </div>
-                  <div className='ml-3 text-sm'>
-                    <label
-                      htmlFor='terms'
-                      className='font-light text-gray-500 dark:text-gray-300'
-                    >
-                      I accept the{' '}
-                      <Link
-                        className='font-medium text-primary-600 hover:underline dark:text-primary-500'
-                        to='#'
-                      >
-                        Terms and Conditions
-                      </Link>
-                    </label>
-                  </div>
+                <div className='text-right mt-0'>
+                  <button
+                    type='button'
+                    className=' mb-2 text-sm font-medium text-gray-900 dark:text-white'
+                    onClick={togglePasswordVisibility}
+                  >
+                    {showPassword ? 'Hide password' : 'Show password'}
+                  </button>
                 </div>
-
                 <button
                   type='submit'
                   className='w-full text-white bg-kalegreen-600 hover:bg-kalegreen-500 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-800'
                 >
                   Create an account
                 </button>
-
                 <p className='text-sm font-light text-gray-500 dark:text-gray-400'>
                   Already have an account?{' '}
                   <Link
