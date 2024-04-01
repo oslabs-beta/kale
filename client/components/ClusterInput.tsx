3cedd8b0008cca2ff22d203e302add6d5af61928
Sonia Han <36926546+soheunhan@users.noreply.github.com>
2024-04-01 10:22:14 -0400

    end to end testing in progress

--- client/components/ClusterInput.tsx
+++ client/components/ClusterInput.tsx
@@ -33,10 +33,10 @@ const ClusterInput = (props: ClusterInputProps) => {
           <label className="block mb-2 text-sm font-medium text-zinc-900 dark:text-white"></label>
           <input
             type="text"
-            id="default-input"
+            id="url-input"
             placeholder="Your Prometheus URL"
             onChange={props.handleInputChange}
-            className=" bg-zinc-50 border border-zinc-300 text-zinc-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-zinc-700 dark:border-zinc-600 dark:placeholder-zinc-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="url-input bg-zinc-50 border border-zinc-300 text-zinc-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-zinc-700 dark:border-zinc-600 dark:placeholder-zinc-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           />
           <Link
             to="/dashboard"
--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,14 +1,22 @@
 import { expect } from 'chai';
 
 describe('My First Test', () => {
-  it('Visits the Kitchen Sink', () => {
-    cy.visit('https://localhost:8080');
-    cy.contains('type').click();
-    cy.url().should('include', '/commands/actions');
-    // Get an input, type into it
-    cy.get('.action-email').type('fake@email.com');
+  beforeEach(() => {
+    cy.visit('http://localhost:8080');
+  });
 
-    //  Verify that the value has been updated
-    cy.get('.action-email').should('have.value', 'fake@email.com');
+  it('has a title', () => {
+    cy.contains('Welcome to kale').click();
+    cy.get('#url-input').type('http://127.0.0.1:8081/');
+    cy.get('#url-input').should('have.value', 'http://127.0.0.1:8081/');
+
+    // cy.url().should('include', '/commands/actions');
+    // // Get an input, type into it
+
+    // //  Verify that the value has been updated
+  });
+  it('has a "Get starteg" button and input field shows up when clicked', () => {
+    cy.contains('Get started').click();
+    cy.get('#url-input').should('be.visible');
   });
 });
