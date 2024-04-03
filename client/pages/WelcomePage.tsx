4dd4e072ac1cf2d265faf3e8ea6ca57f6e3dc74b
Sonia Han <36926546+soheunhan@users.noreply.github.com>
2024-04-02 22:48:14 -0400

    added more padding to the top on the welcome page

--- client/pages/WelcomePage.tsx
+++ client/pages/WelcomePage.tsx
@@ -40,7 +40,7 @@ export default function WelcomePage() {
   return (
     <>
       <NavBar title="Home" to="/" />
-      <div className="relative h-5/6 isolate pt-14 ">
+      <div className="relative h-5/6 isolate pt-24">
         <div className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-96">
           <div
             className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#3AD48F] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
